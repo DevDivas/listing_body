c462cf278e047ab8ecddf0cdc45d90a72be8823a
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-07-20 11:58:10 -0700

    Refactor data generator for airbnb style compliance

--- database/generate-data.js
+++ database/generate-data.js
@@ -1,19 +1,16 @@
 const fs = require('fs');
-const data = require('./data.js');
 const faker = require('faker');
-// generate data
-// write to csv file
+const data = require('./data.js');
 
 const generateData = (field, fileName) => {
   // i: string, and string
   // o: side effect, writes field content to fileName
 
-
   const info = Array.isArray(field) ? field.join('\n') : field.split('\n').join('\n');
 
   fs.writeFile(`${fileName}.csv`, info, 'utf8', (err) => {
     if (err) {
-      console.log('there was an error!');
+      throw Error;
     } else {
       console.log('write was successful!');
     }
@@ -23,20 +20,15 @@ const generateData = (field, fileName) => {
 const getRandomIndex = max => Math.floor(Math.random() * (max + 1));
 
 const capitalizeFirstLetter = (word) => {
-  // i: string, word
-  // o: string, word with first letter capitalized
 
-  let first = word[0];
-  let remainder = word.slice(1);
+  const first = word[0];
+  const remainder = word.slice(1);
 
   return first.toUpperCase() + remainder;
 };
 
 const generateExperience = () => {
-  // i: nothing
-  // o: string
-
-  let result = ``;
+  let result = '';
 
   for (let i = 0; i <= 100; i += 1) {
     let adjective1 = data.adjectives.split('\n')[getRandomIndex(32)];
@@ -54,7 +46,6 @@ const generateExperience = () => {
 };
 
 const generateFakeNames = () => Array(100).fill('').map(faker.name.firstName);
-const generateFakeAddress = () => Array(100).fill('').map(faker.address.city);
 
 generateData(generateFakeNames(), 'host-names');
 generateData(data.cities, 'cities');
