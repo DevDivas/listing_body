944624fc1b1025132810f10098eb76a7055eaa04
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-07-22 17:46:04 -0700

    Refactor data generator to output in CSV format

--- database/data.js
+++ database/data.js
@@ -146,13 +146,64 @@ Ohoopee, Washington
 Mauzy, Mississippi
 Mashpee, California
 Hopatcong, Kansas
+Notrees, Montana
+Honolulu, Maryland
+Overshot, New York
+Put-in-Bay, Alabama
+Homer, Colorado
+Boiling Point, Texas
+Coffee, Maine
+Dollar Point, Rhode Island
+Shisholop, Oklahoma
+Whiskey Spring, California
+Burnt Woods, Colorado
+Breidablick, Delaware
+Sammamish, Oregon
+Steamboat Springs, Arizona
+Thistledown, Arizona
+Wildcat, Nebraska
+Cape Horn, District of Columbia
+Bearcreek, Massachusetts
+Deep Tunnel, Kentucky
+Christmas City, Ohio
+Koosharem, Arizona
+Bright Star, District of Columbia
+Bald Knob, California
+Figure Five, Georgia
+Frisbee, Connecticut
+Possum Grape, Missouri
+Rocky Comfort, Maryland
+Thrasher, Ohio
+Bone Gap, Colorado
+Kankakee, Colorado
+Cricket, Massachusetts
+Neon, Tennessee
+China, Delaware
+Blackhoof, Georgia
+Eden Prairie, Ohio
+New Munich, Missouri
+Poor Farm Landing, Oklahoma
+Improve, District of Columbia
+Six Towns, West Virginia
+Ko Ko, South Carolina
+Tuckaleechee, Louisiana
+Ben Arnold, Indiana
+Guys Store, West Virginia
+Notrees, Montana
+Sempronius, Alabama
+Eau Claire, Minnesota
+Little Heaven, Missouri
+Ohoopee, Washington
+Mauzy, Mississippi
+Mashpee, California
+Hopatcong, Kansas
 Honolulu, Maryland
 Overshot, New York
 Put-in-Bay, Alabama`;
 
 const adjectives = `marvelous
 therapeutic
-next
+great
 nebulous
 offbeat
 quaint
@@ -163,6 +214,7 @@ happy
 intimate
 petite
 jazzy
+refreshing
 electrifying
 exhilarating
 delightful
@@ -170,7 +222,7 @@ sensational
 animating
 stimulating
 vitalizing
-overjoyed
+joyful
 euphoric
 jubilant
 immersing
@@ -187,11 +239,12 @@ pleasure`;
 const nouns = `home
 abode
 pad
-local
+locale
+spot
 house
 experience
 destination
-vacation home
+vacation Home
 cabin
 sight
 house
--- database/generate-data.js
+++ database/generate-data.js
@@ -2,51 +2,137 @@ const fs = require('fs');
 const faker = require('faker');
 const data = require('./data.js');
 
-const generateData = (field, fileName) => {
-  // i: string, and string
-  // o: side effect, writes field content to fileName
+const capitalizeFirstLetter = word => word[0].toUpperCase() + word.slice(1);
 
-  const info = Array.isArray(field) ? field.join('\n') : field.split('\n').join('\n');
+const getRandomIndex = max => Math.floor(Math.random() * (max - 1 + 1)) + 1;
 
-  fs.writeFile(`${fileName}.csv`, info, 'utf8', (err) => {
-    if (err) {
-      throw Error;
-    } else {
-      console.log('write was successful!');
-    }
-  });
-};
-
-const getRandomIndex = max => Math.floor(Math.random() * (max + 1));
-
-const capitalizeFirstLetter = (word) => {
-
-  const first = word[0];
-  const remainder = word.slice(1);
-
-  return first.toUpperCase() + remainder;
-};
+const randomBool = () => Math.floor(Math.random() * (1 + 1));
 
 const generateExperience = () => {
   let result = '';
 
   for (let i = 0; i <= 100; i += 1) {
-    let adjective1 = data.adjectives.split('\n')[getRandomIndex(32)];
-    adjective1 = capitalizeFirstLetter(adjective1);
-
-    let adjective2 = data.adjectives.split('\n')[getRandomIndex(32)];
-    adjective2 = capitalizeFirstLetter(adjective2);
-
-    let noun = data.nouns.split('\n')[getRandomIndex(12)];
-    noun = capitalizeFirstLetter(noun);
+    const adjective1 = capitalizeFirstLetter(data.adjectives.split('\n')[getRandomIndex(32)]);
+    const adjective2 = capitalizeFirstLetter(data.adjectives.split('\n')[getRandomIndex(32)]);
+    const noun = capitalizeFirstLetter(data.nouns.split('\n')[getRandomIndex(12)]);
 
     result += `${adjective1} ${adjective2} ${noun}\n`;
   }
   return result;
 };
 
-const generateFakeNames = () => Array(100).fill('').map(faker.name.firstName);
+const generateListing = () => {
+  const results = [];
+  const citiesAndStates = data.cities.split('\n').map(cityState => cityState.split(','));
+  const experience = generateExperience().split('\n');
 
-generateData(generateFakeNames(), 'host-names');
-generateData(data.cities, 'cities');
-generateData(generateExperience(), 'listing-name');
+  for (let i = 0; i <= 100; i += 1) {
+    const num = getRandomIndex(8);
+    results.push(`${i + 1},`
+      + `${experience[i]},`
+      + `${citiesAndStates[i][0]},`
+      + `${citiesAndStates[i][1]},`
+      + `${faker.name.firstName()},`
+      + `${num},`
+      + `${getRandomIndex(num)},`
+      + `${getRandomIndex(num - 1)}`);
+  }
+  return results.join('\n');
+};
+const generateHomeHighlights = () => {
+  const results = [];
+
+  
+  for (let i = 0; i <= 100; i += 1) {
+    const adjective = capitalizeFirstLetter(data.adjectives.split('\n')[getRandomIndex(32)]);
+    const noun = capitalizeFirstLetter(data.nouns.split('\n')[getRandomIndex(12)]);
+
+    const header = `${adjective} ${noun}`;
+    const helpful = randomBool(1);
+    results.push(`${i + 1},`
+      + `${i + 1},`
+      + `${header},`
+      + `${faker.lorem.sentence()},`
+      + `${helpful},`
+      + `${!helpful}`);
+  }
+  return results.join('\n');
+};
+
+const generateHouseRules = () => {
+  const results = [];
+
+  for (let i = 0; i <= 100; i += 1) {
+    results.push(`${i + 1}
+      ${randomBool(1)}
+      ${randomBool(1)}
+      ${randomBool(1)}
+      ${randomBool(1)}
+      3
+      12
+      ${faker.lorem.paragraph()}
+      ${randomBool(1)}
+      ${randomBool(300)}`.split('\n').join(','));
+  }
+  return results.join('\n');
+};
+
+const generateAmenities = (num) => {
+  let results = '';
+
+  for (let i = 0; i < 100; i += 1) {
+    let amenityStr = `${i + 1},`;
+    for (let j = 0; j <= num; j += 1) {
+      const trail = j === num ? '\n' : ',';
+      amenityStr += `${randomBool()}${trail}`;
+    }
+    results += amenityStr.includes('\n') ? amenityStr : `${amenityStr}\n`;
+  }
+  return results;
+};
+
+const generateAmenitiesTable = (num) => {
+  let results = '';
+
+  for (let i = 0; i < 100; i += 1) {
+    let amenityStr = `${i + 1},`;
+    for (let j = 0; j < num; j += 1) {
+      const trail = j === num ? '\n' : ',';
+      amenityStr += `${i + 1}${trail}`;
+    }
+    results += amenityStr.includes('\n') ? amenityStr : `${amenityStr}\n`;
+  }
+  return results;
+};
+
+const generateFile = (field, fileName) => {
+  const info = Array.isArray(field) ? field.join('\n') : field.split('\n').join('\n');
+
+  fs.writeFile(`./data/${fileName}.csv`, info, 'utf8', (err) => {
+    if (err) {
+      throw Error(err);
+    } else {
+      console.log('write was successful!');
+    }
+  });
+};
+
+const amenities = ['basic', 'facilities', 'guest_access', 'dining', 'bed_and_bath', 'safety_features'];
+const catNums = [7, 3, 2, 1, 5, 3];
+
+amenities.forEach((amenity, i) => {
+  generateFile(generateAmenities(catNums[i]), amenity);
+});
+
+generateFile(generateListing(), 'listing');
+generateFile(generateHomeHighlights(), 'home-highlights');
+generateFile(generateHouseRules(), 'house-rules');
+generateFile(generateAmenitiesTable(7), 'amenities');
+
+// const generateFakeNames = () => Array(100).fill('').map(faker.name.firstName);
+
+// generateData(generateFakeNames(), 'host-names');
+// generateData(data.cities, 'cities');
+// generateData(generateExperience(), 'listing-name');
+
+// console.log(generateListing());
