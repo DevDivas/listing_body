6c6c9f447662e41f9b251b4ffed85f9f4730d52c
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-07-24 15:59:21 -0700

    Refactor server database response format

--- server/server.js
+++ server/server.js
@@ -18,39 +18,76 @@ connection.connect();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/rooms/', (req, res) => {
+app.get('/rooms/:roomId', (req, res) => {
 
-  const room = req.query.roomid;
+  console.log(req.params.roomId);
+  const room = req.params.roomId;
 
-  let query = `SELECT * FROM listing, 
-    home_highlights, 
-    house_rules, 
-    basic, 
-    facilities, 
-    guest_access, 
-    dining, 
-    bed_and_bath, 
-    safety_features WHERE 
-    listing.id IN (${room}) 
-    AND home_highlights.id IN (${room}) 
-    AND house_rules.id IN (${room}) 
-    AND basic.id IN (${room}) 
-    AND facilities.id IN (${room}) 
-    AND guest_access.id IN (${room}) 
-    AND dining.id IN (${room}) 
-    AND bed_and_bath.id IN (${room}) 
-    AND safety_features.id IN (${room})`;
+  const response = {};
+  const tables = [
+    'listing', 
+    'home_highlights', 
+    'basic', 
+    'facilities', 
+    'guest_access',
+    'dining',
+    'bed_and_bath',
+    'safety_features'
+  ];
 
-  connection.query(query, (err, rows, fields) => {
-    if (err) {
-      throw Error(err);
-    } else {
-      // console.log(rows);
-      res.send(rows);
-    }
+  const queryTables = (table, room) => {
+    let query = `SELECT * FROM ${table} WHERE id IN (${room});`;
+    // console.log(query);
+    connection.query(query, (err, rows, fields) => {
+      if (err) {
+        throw Error(err)
+      } else {
+        response[table] = JSON.parse(JSON.stringify(rows[0]));
+        if(Object.keys(response).length === tables.length) {
+          console.log([response]);
+          res.send([response]);
+        }
+      }
+    });
+  };
+
+  tables.forEach((table) => {
+    queryTables(table, room);
+    // console.log(response);
   });
+
+  // res.send(response);
+  // let query = `SELECT * FROM listing, 
+  //   home_highlights, 
+  //   house_rules, 
+  //   basic, 
+  //   facilities, 
+  //   guest_access, 
+  //   dining, 
+  //   bed_and_bath, 
+  //   safety_features WHERE 
+  //   listing.id IN (${room}) 
+  //   AND home_highlights.id IN (${room}) 
+  //   AND house_rules.id IN (${room}) 
+  //   AND basic.id IN (${room}) 
+  //   AND facilities.id IN (${room}) 
+  //   AND guest_access.id IN (${room}) 
+  //   AND dining.id IN (${room}) 
+  //   AND bed_and_bath.id IN (${room}) 
+  //   AND safety_features.id IN (${room})`;
+
+  // connection.query(query, (err, rows, fields) => {
+  //   if (err) {
+  //     throw Error(err);
+  //   } else {
+  //     // console.log(rows);
+  //     res.send(rows);
+  //   }
+  // });
   
+
 })
 
 // connection.end();
