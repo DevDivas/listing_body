3577be42b0d00f26e65e94b367cdbc0b992e7ee6
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-07-30 11:52:39 -0700

    Add jest params into eslint config

--- database/schema.sql
+++ database/schema.sql
@@ -17,7 +17,6 @@ CREATE TABLE `listing` (
 
 CREATE TABLE `home_highlights` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `room_id` INT,
   `header1` VARCHAR(30),
   `header2` VARCHAR(30),
   `header3` VARCHAR(30),
@@ -35,7 +34,6 @@ CREATE TABLE `home_highlights` (
 
 CREATE TABLE `house_rules` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  room_id INT,
   `suitable_for_children` BOOLEAN,
   `smoking` BOOLEAN,
   `pets` BOOLEAN, 
@@ -50,7 +48,6 @@ CREATE TABLE `house_rules` (
 
 CREATE TABLE `amenities` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `room_id` INT,
   `basic` INT,
   `facilities` INT,
   `guest_access` INT,
@@ -62,7 +59,6 @@ CREATE TABLE `amenities` (
 
 CREATE TABLE `basic` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  room_id INT,
   `wifi` BOOLEAN,
   `washer` BOOLEAN,
   `dryer` BOOLEAN,
@@ -75,7 +71,6 @@ CREATE TABLE `basic` (
 
 CREATE TABLE `facilities` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  room_id INT,
   `parking` BOOLEAN,
   `elevator` BOOLEAN,
   `hot_tub` BOOLEAN,
@@ -84,7 +79,6 @@ CREATE TABLE `facilities` (
 
 CREATE TABLE `guest_access` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  room_id INT,
   `host_greets` BOOLEAN,
   `lockbox` BOOLEAN,
   PRIMARY KEY (`id`)
@@ -92,14 +86,12 @@ CREATE TABLE `guest_access` (
 
 CREATE TABLE `dining` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  room_id INT,
   `kitchen` BOOLEAN,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `bed_and_bath` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  room_id INT,
   `hair_dryer` BOOLEAN,
   `hangers` BOOLEAN,
   `shampoo` BOOLEAN,
@@ -110,7 +102,6 @@ CREATE TABLE `bed_and_bath` (
 
 CREATE TABLE `safety_features` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  room_id INT,
   `fire_extinguisher` BOOLEAN,
   `smoke_detector` BOOLEAN,
   `first_aid` BOOLEAN,
@@ -118,20 +109,11 @@ CREATE TABLE `safety_features` (
 );
 
 ALTER TABLE `amenities` ADD FOREIGN KEY (`basic`) REFERENCES `basic`(`id`);
-ALTER TABLE `amenities` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
 ALTER TABLE `amenities` ADD FOREIGN KEY (`facilities`) REFERENCES `facilities`(`id`);
 ALTER TABLE `amenities` ADD FOREIGN KEY (`guest_access`) REFERENCES `guest_access`(`id`);
 ALTER TABLE `amenities` ADD FOREIGN KEY (`dining`) REFERENCES `dining`(`id`);
 ALTER TABLE `amenities` ADD FOREIGN KEY (`bed_and_bath`) REFERENCES `bed_and_bath`(`id`);
 ALTER TABLE `amenities` ADD FOREIGN KEY (`safety_features`) REFERENCES `safety_features`(`id`);
-
-ALTER TABLE `house_rules` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
-ALTER TABLE `basic` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
-ALTER TABLE `facilities` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
-ALTER TABLE `guest_access` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
-ALTER TABLE `dining` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
-ALTER TABLE `bed_and_bath` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
-ALTER TABLE `safety_features` ADD FOREIGN KEY (`room_id`) REFERENCES `listing`(`id`);
 
 LOAD DATA LOCAL INFILE './database/data/listing.csv'
 INTO TABLE listing
