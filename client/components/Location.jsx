78212b44427dcf4c3eb81b94457bf97be80f5117
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2018-09-17 00:27:48 -0400

    Update Location component with new path to api key

--- client/components/Location.jsx
+++ client/components/Location.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import apiKey from '../../server/apiKey';
+import apiKey from '../../keys/googleMapsEmbedKey';
 
 const Location = ({ city, state }) => (
   <div id="location">
-    {console.log(`${city.split(' ').join('%20')}+${state.split(' ').join('%20')}`)}
     <iframe title="location" width="600" height="450" frameBorder="0" src={`https://www.google.com/maps/embed/v1/place?key=${apiKey}&q=${city.split(' ').join('%20')}+${state.split(' ').join('%20')}`} allowFullScreen />
   </div>
 );
